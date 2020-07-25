57dc5c0b921d4293de357b33962ddc5786085fbf
keitakn <keita.koga.work@gmail.com>
2020-07-25 23:29:35 +0900

    :green_heart: #10 CIのテストが終わったのでterraform fmt -recursiveの結果をコミット

--- providers/aws/environments/prod/20-frontend/backend.tf
+++ providers/aws/environments/prod/20-frontend/backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket  = "prod-nekochans-portfolio-tfstate"
     key     = "frontend/terraform.tfstate"
     region  = "ap-northeast-1"
-    profile   = "nekochans-portfolio"
+    profile = "nekochans-portfolio"
   }
 }
