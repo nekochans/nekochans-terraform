f92ddcf144f70c0e9669e0b77e0987593162417b
keitakn <keita.koga.work@gmail.com>
2021-10-19 14:44:35 +0900

    :fire: #24 nekochans.org はvercelでホスティングするように改修し不要なリソースは削除

--- modules/aws/frontend/route53.tf
+++ modules/aws/frontend/route53.tf
@@ -2,16 +2,13 @@ data "aws_route53_zone" "main" {
   name = var.main_domain_name
 }
 
-resource "aws_route53_record" "frontend" {
+resource "aws_route53_record" "frontend_apex_domain" {
+  count   = var.env == "prod" ? 1 : 0
   zone_id = data.aws_route53_zone.main.zone_id
-  name    = var.env == "prod" ? var.main_domain_name : var.sub_domain_name
+  name    = var.main_domain_name
   type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.frontend.domain_name
-    zone_id                = aws_cloudfront_distribution.frontend.hosted_zone_id
-    evaluate_target_health = false
-  }
+  records = ["76.76.21.21"]
+  ttl     = "5"
 }
 
 // ここから下は Next.js 版に変わった後でも残す
